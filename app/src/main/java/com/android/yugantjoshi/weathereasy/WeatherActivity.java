39073c6e1c961e31ce4a52ea16c58722c81ce747
Yugant Joshi <yugantjoshi@Yugants-MacBook-Pro.local>
2016-05-19 16:05:20 -0400

    Revamped UI now with time and date

--- app/src/main/java/com/android/yugantjoshi/weathereasy/WeatherActivity.java
+++ app/src/main/java/com/android/yugantjoshi/weathereasy/WeatherActivity.java
@@ -1,6 +1,10 @@
 package com.android.yugantjoshi.weathereasy;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.location.Address;
@@ -12,6 +16,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,8 +35,12 @@ import net.steamcrafted.materialiconlib.MaterialIconView;
 import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -45,6 +56,13 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
     protected static final String TAG = "MainActivity";
 
+    //TextViews
+    @Bind(R.id.time_text)
+    TextView time_text;
+    @Bind(R.id.day_text)
+    TextView day_text;
+    @Bind(R.id.date_text)
+    TextView date_text;
     @Bind(R.id.temp_text)
     TextView temp_text;
     @Bind(R.id.humidity_text)
@@ -53,32 +71,62 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     TextView city_text;
     @Bind(R.id.wind_text)
     TextView wind_text;
+    //Icons
     @Bind(R.id.weather_icon)
     MaterialIconView weather_icon;
-    @Bind(R.id.water_icon)
-    MaterialIconView water_icon;
-    // @Bind(R.id.rain_percentage) TextView rain_percentage;
 
-    Typeface lightFont;
     double latitude, longitude;
 
     private static final int REQUEST_LOCATION = 1888;
     protected GoogleApiClient googleApiClient;
     protected Location lastLocation;
 
+    BroadcastReceiver _broadcastReceiver;
+    private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("H:mm a");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        permissionChecks();
+        buildGoogleApiClient();
 
         ButterKnife.bind(this);
-        lightFont = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Light.ttf");
+        setDate(day_text, date_text);
 
-        temp_text.setTypeface(lightFont);
-        city_text.setTypeface(lightFont);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm a");
+        time_text.setText(dateFormat.format(new Date()).toString());
 
+        setFont(day_text, "OpenSans-Light.ttf");
+        setFont(time_text, "OpenSans-Light.ttf");
+        setFont(date_text, "OpenSans-Light.ttf");
+        setFont(temp_text, "OpenSans-Light.ttf");
+        setFont(city_text, "OpenSans-Light.ttf");
+        setFont(humidity_text, "OpenSans-Light.ttf");
+        setFont(wind_text, "OpenSans-Light.ttf");
+    }
 
+    private void setDate(TextView currentDay, TextView fullDate)
+    {
+        Log.d("Time","Getting Current Time");
+        TimeZone t = TimeZone.getDefault();
+        Calendar c = Calendar.getInstance(t);
+        Locale locale = Locale.getDefault();
+
+        //Day and full Date
+        currentDay.setText(c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale));
+        int year = c.get(Calendar.YEAR);
+        String month = c.getDisplayName(Calendar.MONTH, Calendar.LONG, locale);
+        int dayNumber = c.get(Calendar.DATE);
+        fullDate.setText(month+" "+dayNumber+", "+year);
+    }
+    public void setFont(TextView textView, String font)
+    {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/"+font);
+        textView.setTypeface(typeface);
+    }
+    public void permissionChecks()
+    {
         Log.d("Connected", "Check Permissions");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
@@ -93,9 +141,6 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
             }
         }
-
-        buildGoogleApiClient();
-
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -128,9 +173,7 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     }
 
 
-    /*
-    Loads the proper weather icon based on description
-     */
+   //Loads weather icon based on conditions
     private void changeWeatherIcon(String conditions) {
         //weather_icon.setIcon(MaterialDrawableBuilder.IconValue.WEATHER_CLOUDY);
         Log.d("Weather Icon", conditions);
@@ -162,10 +205,11 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
                 double temp = response.body().getMain().getTemp();
                 //Convert temp to fahrenheit
                 temp = temp * (9.0 / 5) - 459.67;
-                String tempConv = String.format("%.1f", temp);
+                String tempConv = String.format("%.0f", temp);
                 double wind = response.body().getWind().getSpeed();
-                String windConv = String.format("%.1f", wind);
+                String windConv = String.format("%.0f", wind);
                 String conditions = response.body().getWeather().get(0).getMain();
+                
 
                 changeWeatherIcon(conditions);
 
@@ -176,7 +220,6 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
 
             @Override
             public void onFailure(Call<WeatherData> call, Throwable t) {
-                Toast.makeText(WeatherActivity.this, R.string.update_fail, Toast.LENGTH_LONG).show();
                 t.printStackTrace();
             }
         });
@@ -211,12 +254,21 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
             Toast.makeText(this, R.string.update_fail, Toast.LENGTH_LONG).show();
         }
     }
-
     @Override
     public void onStart() {
 
         super.onStart();
         googleApiClient.connect();
+        _broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    time_text.setText(_sdfWatchTime.format(new Date()));
+            }
+        };
+
+        registerReceiver(_broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+
     }
 
     @Override
@@ -226,6 +278,13 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
         if (googleApiClient.isConnected()) {
             googleApiClient.disconnect();
         }
+    }
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        if (_broadcastReceiver != null)
+            unregisterReceiver(_broadcastReceiver);
     }
 
     @Override
@@ -237,6 +296,4 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     public void onConnectionSuspended(int i) {
 
     }
-
-
 }
